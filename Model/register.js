7283050563685250cc7a60d119b1296224c6d677
Yash Parihar <yashparihar760@gmail.com>
2025-08-22 18:29:53 +0530

    update

--- Model/register.js
+++ Model/register.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Counter = require("./counter");
 
 const user =   new mongoose.Schema({
     email:{
@@ -21,12 +22,29 @@ const user =   new mongoose.Schema({
         type:String
 
     },
+    role: {
+        type:String,
+        default: "customer"
+
+    },
      registerTime: {
         type: Date,
         default: Date.now
     }
     
 
+});
+user.pre("save", async function (next) {
+  if (this.isNew) {
+    const counter = await Counter.findOneAndUpdate(
+      { id: "userId" },
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true }
+    );
+
+    this.userId = `userid-${counter.seq}`;
+  }
+  next();
 });
 
 const users = mongoose.model('users' , user);
